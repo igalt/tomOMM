8ab6c63e5c77b533dda2ecc14d20e300af2d51f4
oraab <oraab@ebay.com>
2016-06-21 16:53:11 +0300

    adding quickview API for users, adding relevant new fields to model and to HTML template for the profile edit.

--- modules/users/server/routes/users.server.routes.js
+++ modules/users/server/routes/users.server.routes.js
@@ -11,6 +11,15 @@ module.exports = function (app) {
   app.route('/api/users/password').post(users.changePassword);
   app.route('/api/users/picture').post(users.changeProfilePicture);
 
+  app.route('/api/users/quickview/:userId').get(function (req,res) {
+    var responseObj = {"name": req.model.firstName+" "+req.model.lastName,
+                       "image": req.model.profileImageURL,
+                       "description": req.model.description};
+    res.json(responseObj);
+    console.log(res);
+
+  });
+
   // Finish by binding the user middleware
   app.param('userId', users.userByID);
 };
